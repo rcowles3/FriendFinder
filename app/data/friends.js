1781c3d56267ddcde72304246323dd383d53736e
rcowles3 <ryan.cowles3@gmail.com>
2017-07-21 10:44:03 -0700

    lots of work, still neds to be done, once have access to internet again.

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// ===============================================================================
+// FRIEND DATA
+// ===============================================================================
+
+var compatibleFriends = {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+};
+
+// Exporting compatableFriends object to make accessable elsewhere. 
+module.exports = compatibleFriends;
