3d0add9829f0efdfc72c78d2c19153661996617d
rcowles3 <ryan.cowles3@gmail.com>
2017-07-29 00:25:47 -0700

    need to review class recordings and activites

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,17 @@
 // FRIEND DATA
 // ===============================================================================
 
-var compatibleFriends = {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-};
+var compatibleFriends = [{
+        "name": "SpongeBob",
+        "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiL3pK1863VAhXIgFQKHQZZAqkQjRwIBw&url=https%3A%2F%2Fwww.thoughtco.com%2Fbest-spongebob-squarepants-episodes-137682&psig=AFQjCNF03kVSXQCYdSkDb5z3MAaIUL1X4Q&ust=1501397403098080",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "SpiderMan",
+        "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiXkceV9q3VAhVmrFQKHVomAj0QjRwIBw&url=http%3A%2F%2Fmarvel.com%2Fcharacters%2F54%2Fspider-man&psig=AFQjCNFCZv3d7UR7qTbl3uyntOVd873IpA&ust=1501398166235430",
+        "scores": [2, 2, 3, 3, 3, 4, 1, 3, 5, 4]
+    }
+];
 
 // Exporting compatableFriends object to make accessable elsewhere. 
 module.exports = compatibleFriends;
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // ================================================================================
-// ROUTER
+// ROUTERS
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
